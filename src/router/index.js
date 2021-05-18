b01bad34effd18ed8db427f8250e8f64fe50cb79
Sora <soraxas@163.com>
2021-05-18 11:21:18 +0800

    yeb 2021-05-18 前端完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,46 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from "@/views/Login";
+import Home from "@/views/Home";
+import FriendChat from "@/views/chat/FriendChat";
+import AdminInfo from "@/views/AdminInfo";
 
 Vue.use(VueRouter)
+// export default new VueRouter({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'Login',
+//       component: Login,
+//       hidden: true
+//     }
+//   ]
+// })
+
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Login',
+    component: Login,
+    hidden: true
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/home',
+    name: 'Home',
+    component: Home,
+    children: [
+      {
+        path: '/chat',
+        name: '在线聊天',
+        component: FriendChat,
+      },
+      {
+        path: '/userinfo',
+        name: '个人中心',
+        component: AdminInfo,
+      }
+    ]
   }
 ]
 
